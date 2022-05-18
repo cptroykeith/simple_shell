d7ea7856850df8ab418bd33d570998c0bff41be4
g091 <gotieno791@gmail.com>
2022-05-19 02:01:21 +0300

    rm

--- builtins.c
+++ builtins.c
@@ -5,14 +5,13 @@
  * @vars: variables
  * Return: pointer to the function or NULL
  */
+
 void (*check_for_builtins(vars_t *vars))(vars_t *vars)
 {
 	unsigned int i;
 	builtins_t check[] = {
 		{"exit", new_exit},
 		{"env", _env},
-		{"setenv", new_setenv},
-		{"unsetenv", new_unsetenv},
 		{NULL, NULL}
 	};
 
@@ -31,6 +30,7 @@ void (*check_for_builtins(vars_t *vars))(vars_t *vars)
  * @vars: variables
  * Return: void
  */
+
 void new_exit(vars_t *vars)
 {
 	int status;
@@ -62,6 +62,7 @@ void new_exit(vars_t *vars)
  * @vars: struct of variables
  * Return: void.
  */
+
 void _env(vars_t *vars)
 {
 	unsigned int i;
@@ -71,87 +72,5 @@ void _env(vars_t *vars)
 		_puts(vars->env[i]);
 		_puts("\n");
 	}
-	vars->status = 0;
-}
-
-/**
- * new_setenv - create a new environment variable, or edit an existing variable
- * @vars: pointer to struct of variables
- *
- * Return: void
- */
-void new_setenv(vars_t *vars)
-{
-	char **key;
-	char *var;
-
-	if (vars->av[1] == NULL || vars->av[2] == NULL)
-	{
-		print_error(vars, ": Incorrect number of arguments\n");
-		vars->status = 2;
-		return;
-	}
-	key = find_key(vars->env, vars->av[1]);
-	if (key == NULL)
-		add_key(vars);
-	else
-	{
-		var = add_value(vars->av[1], vars->av[2]);
-		if (var == NULL)
-		{
-			print_error(vars, NULL);
-			free(vars->buffer);
-			free(vars->commands);
-			free(vars->av);
-			free_env(vars->env);
-			exit(127);
-		}
-		free(*key);
-		*key = var;
-	}
-	vars->status = 0;
-}
-
-/**
- * new_unsetenv - remove an environment variable
- * @vars: pointer to a struct of variables
- *
- * Return: void
- */
-void new_unsetenv(vars_t *vars)
-{
-	char **key, **newenv;
-
-	unsigned int i, j;
-
-	if (vars->av[1] == NULL)
-	{
-		print_error(vars, ": Incorrect number of arguments\n");
-		vars->status = 2;
-		return;
-	}
-	key = find_key(vars->env, vars->av[1]);
-	if (key == NULL)
-	{
-		print_error(vars, ": No variable to unset");
-		return;
-	}
-	for (i = 0; vars->env[i] != NULL; i++)
-		;
-	newenv = malloc(sizeof(char *) * i);
-	if (newenv == NULL)
-	{
-		print_error(vars, NULL);
-		vars->status = 127;
-		new_exit(vars);
-	}
-	for (i = 0; vars->env[i] != *key; i++)
-		newenv[i] = vars->env[i];
-	for (j = i + 1; vars->env[j] != NULL; j++, i++)
-		newenv[i] = vars->env[j];
-	newenv[i] = NULL;
-	free(*key);
-	free(vars->env);
-	vars->env = newenv;
 	vars->status = 0;
 }
