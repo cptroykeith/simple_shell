5f0fe267d1e68a346f12737bead424a6f7ed6672
g091 <gotieno791@gmail.com>
2022-05-18 17:46:00 +0300

    prototypes updated

--- shell.h
+++ shell.h
@@ -29,16 +29,26 @@ void __exit(char **tokens, char **history, int fd);
 void _env(char **tokens);
 char *check_path(char *file);
 int check_file(char *dirname, char *file);
+
 int _atoi(char *str);
+
 char *_strdup(char *str);
 int  _strlen(char *str);
 int _strcmp(const char *s1, const char *s2);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *s1, char *s2);
+
 void help(char **tokens);
 void sighandler(int sig_n);
+
 void printhistory(char **history);
 void updatehistory(char **history, int fd);
+
+char *_strtok(char *str, char *delim);
+int isdelim(char ch, char *delim);
+ssize_t _getline(char **lineptr, size_t *n, int fd);
+void *_realloc(void *ptr, size_t old_size, size_t new_size);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* _SHELL_H_ */
 
